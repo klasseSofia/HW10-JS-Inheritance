f39fe47195cf943cff726782264b9fccbcf72bf4
Sofia <honey.klassen1498@gmail.com>
2017-07-16 15:41:33 +0300

    HW10-JS-Inheritance: change js file

--- main.js
+++ main.js
@@ -1,44 +1,47 @@
 /**
  * Created by Super on 13.07.2017.
  */
-var Gladiators = function () {
-    this.name = 'not name';
-    this.attack = 0;
-    this.hitpoints = Math.floor(Math.random() * (100 - 80 + 1)) + 80;
-  this.fight = function (enemy) {
-      while (this.hitpoints != 0 || enemy.hitpoints != 0) {
-          if (this.attack > enemy.attack) {
-              enemy.hitpoints -= enemy.attack;
-          } else {
-              this.hitpoints -= this.attack;
+    var Gladiators = function () {
+        this.name = 'not name';
+        this.attack = 0;
+        this.hitpoints = Math.floor(Math.random() * (100 - 80 + 1)) + 80;
+        this.fight = function (enemy) {
+          while (this.hitpoints != 0 || enemy.hitpoints != 0) {
+
+              if (this.attack > enemy.attack) {
+                  enemy.hitpoints -= enemy.attack;
+              } else {
+                  this.hitpoints -= this.attack;
+              }
           }
-      }
-      if(this.hitpoints === 0) {
-          console.log('You died')
-      } else {console.log('Your enemy died')}
-  };
-  this.setAttack = function () {
-      this.attack = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-  }
-  this.setHitpoints = function () {
-      if(hitpoints === 0) {
-          console.log('you died')
-      }
-  }
-};
+          if(this.hitpoints === 0) {
+              console.log('You died')
+          } else {console.log('Your enemy died')}
+        };
 
-var Champion = function () {
-    this.name = 'champion';
-};
+        this.setAttack = function () {
+          this.attack = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+        };
 
-var Monster = function () {
-    this.name = 'monster';
-}
+        this.setHitpoints = function () {
+          if(hitpoints === 0) {
+              console.log('you died');
+          }
+        };
+    };
 
-Champion.prototype = new Gladiators();
-Monster.prototype = new Gladiators();
+    var Champion = function () {
+        this.name = 'champion';
+    };
 
-var heracles = new Champion();
-var boar = new Monster();
+    var Monster = function () {
+        this.name = 'monster';
+    };
 
-heracles.fight(boar);
+    Champion.prototype = new Gladiators();
+    Monster.prototype = new Gladiators();
+
+    var heracles = new Champion();
+    var boar = new Monster();
+
+    heracles.fight(boar);
